e17b7a5c9eab999eb2a1368ef861d550d20e523a
iamSibashisSom <sibashis2005@gmail.com>
2024-08-03 20:21:31 +0530

    d

--- src/Add.js
+++ src/Add.js
@@ -3,20 +3,28 @@ import {useState} from 'react'
 import { useHistory } from 'react-router-dom'
 
 const Add = () => {
+    // function format(date) {
+    //     if(date[1]==='/'){
+    //         date='0'+date;
+    //     }
+    //     if(date[4]==='/'){
+    //         date=date.slice(0,3)+'0'+date.slice(3);
+    //     }
+    //     date=date.split('/').join(':');
+    //     return date;
+    // }
     const history=useHistory();
     const [title, setTitle] = useState('');
-    const [deadline, setDeadline] = useState('');
-    const [time, setTime]=useState('');
+    const [deadline, setDeadline] = useState(new Date());
     const [priority, setPriority] = useState('not important');
-    let curDate = new Date().toLocaleDateString('en-IN').slice(0, 10);
-    console.log(new Date().toLocaleDateString('en-IN'));
-    console.log(curDate);
-    let curTime = new Date().toLocaleTimeString('en-IN').slice(0, 4);
+    let curTime = new Date();
     console.log(curTime);
+    console.log(curTime.getTime());
+    console.log(curTime.getTime());
     const onSubmit = (e) => {
         e.preventDefault();
         let tasks = JSON.parse(localStorage.getItem('tasks'));
-        let newTask={[title]:[deadline, time, priority]};
+        let newTask={[title]:[deadline, priority]};
         tasks=Object.assign(newTask, tasks);
         console.log(newTask);
         localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -24,29 +32,32 @@ const Add = () => {
         history.push('/');
     }
   return (
-    <div class="container">
-        <div class="color-filler hide"></div>
+    <div className="container">
+        <div className="color-filler hide"></div>
         <div>
-            <h2 class="header">Add Task</h2>
-            <form onSubmit={onSubmit} class="form">
+            <h2 className="header">Add Task</h2>
+            <form onSubmit={onSubmit} className="form">
                 <div>
                     <label>Title: </label>
                     <input type="text" value={title} placeholder='Title of the task' required onChange={(e) => setTitle(e.target.value)}></input>
                 </div>
                 <div>
-                    <label>Deadline Date: </label>
-                    <input type="date" value={deadline} required onChange={(e) => {
-                        if(e.target.value<curDate){
+                    <label>Deadline: </label>
+                    <input type="datetime-local" value={deadline} required onChange={(e) => {
+                        let dTime=new Date(Date.parse(e.target.value));
+                        if(dTime.getTime()<curTime.getTime()){
+                            console.log(dTime.getTime());
+                            console.log(curTime.getTime());
                             alert("Deadline should be on or after today's date");
                         }
                         else{
-                            setDeadline(e.target.value);
+                            console.log(curTime.getTime());
+                            console.log(dTime.getTime());
+                            console.log(curTime);
+                            //convert back to proper format
+                            setDeadline(dTime);
                         }
                     }}></input>
-                </div>
-                <div>
-                    <label>Time: </label>
-                    <input type="time" value={time} placeholder='23:59' required onChange={(e) => setTime(e.target.value)}></input>
                 </div>
                 <div>
                     <label>Priority:</label>
@@ -56,10 +67,10 @@ const Add = () => {
                         <option value="crucial">Crucial</option>
                     </select>
                 </div>
-                <button type="submit" class="button">Add</button>
+                <button type="submit" className="button">Add</button>
             </form>
         </div>
-        <div class="color-filler hide"></div>
+        <div className="color-filler hide"></div>
     </div>
   )
 }
